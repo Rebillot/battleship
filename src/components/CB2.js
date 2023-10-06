189a2de1cf36ba7dbf89620029baf58fbae60288
Rebillot <jonathan.rebillot@gmail.com>
2023-10-06 12:55:18 -0300

    Enemy ship length rendering correctly

--- src/components/CB2.js
+++ src/components/CB2.js
@@ -5,31 +5,31 @@ import { useTurn } from "./Context/Context.js";
 
 
 const generateRandomShips = () => {
-    const randomShips = [];
-    const availableRows = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    const availableCols = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  
-    for (let i = 0; i < 5; i++) {
-      const shipObject = ShipArray[i];
-      const shipLength = shipObject.length;
-      const randomRow = availableRows.splice(
-        Math.floor(Math.random() * availableRows.length),
-        1
-      )[0];
-      const randomCol = availableCols.splice(
-        Math.floor(Math.random() * availableCols.length),
-        1
-      )[0];
-  
-      randomShips.push({
-        ship: shipObject,
-        length: shipLength,
-        row: randomRow,
-        col: randomCol,
-      });
-    }
-    return randomShips;
-  };
+  const randomShips = [];
+  const availableRows = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const availableCols = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  for (let i = 0; i < 5; i++) {
+    const shipObject = ShipArray[i];
+    const shipLength = shipObject.length;
+    const randomRow = availableRows.splice(
+      Math.floor(Math.random() * availableRows.length),
+      1
+    )[0];
+    const randomCol = availableCols.splice(
+      Math.floor(Math.random() * availableCols.length),
+      1
+    )[0];
+
+    randomShips.push({
+      ship: shipObject,
+      length: shipLength,
+      row: randomRow,
+      col: randomCol,
+    });
+  }
+  return randomShips;
+};
 
 export default function ComputerBoard() {
   const [ships, setShips] = useState([]);
@@ -44,129 +44,145 @@ export default function ComputerBoard() {
     // Check if the given row and col matches any ship's position
     const attackedShipIndex = ships.findIndex((ship) => ship.row === row && ship.col === col);
     if (attackedShipIndex !== -1) {
-        // Create copy of the ships array and update status
-        const updatedShips = [...ships];
-        updatedShips[attackedShipIndex] = { ...updatedShips[attackedShipIndex], status: 'hit' };
-        
- 
-        setShips(updatedShips); // Update the ships array
+      // Create copy of the ships array and update status
+      const updatedShips = [...ships];
+      updatedShips[attackedShipIndex] = { ...updatedShips[attackedShipIndex], status: 'hit' };
+
+
+      setShips(updatedShips); // Update the ships array
     } else {
 
-       setMissedShots([...missedShots, [row, col]]);
-       
-    }
-};
+      setMissedShots([...missedShots, [row, col]]);
 
-const handleSquareClick = (row, col) => {
-  if (currentTurn === "player") {
-    const clickedCoordinates = [row, col];
-
-    if (!coordinatesRef.current.some(coord => coord[0] === row && coord[1] === col)) {
-      coordinatesRef.current = [...coordinatesRef.current, clickedCoordinates];
-      handlePlayerAttack(row, col);
-      setLastClicked(clickedCoordinates);
-      toggleTurn();
-      console.log("computer's turn")
     }
-  }
-};
+  };
+
+  const handleSquareClick = (row, col) => {
+    if (currentTurn === "player") {
+      const clickedCoordinates = [row, col];
+
+      if (!coordinatesRef.current.some(coord => coord[0] === row && coord[1] === col)) {
+        coordinatesRef.current = [...coordinatesRef.current, clickedCoordinates];
+        handlePlayerAttack(row, col);
+        setLastClicked(clickedCoordinates);
+        toggleTurn();
+        console.log("computer's turn")
+      }
+    }
+  };
   const handlePlaceShip = (row, col, shipLength, ships) => {
     const newShips = [...ships];
     const directions = [
       [1, 0],  // right
       [0, 1],  // down 
     ];
-  
+
     for (let dir of directions) {
       const [rowDir, colDir] = dir;
       let canPlaceShip = true;
-  
+
       for (let i = 0; i < shipLength; i++) {
         const newRow = row + i * rowDir;
         const newCol = col + i * colDir;
-  
+
         const isOutOfBounds = newRow < 0 || newRow >= 10 || newCol < 0 || newCol >= 10;
         const isOverlapping = newShips.some((ship) => ship.row === newRow && ship.col === newCol);
-  
+
         if (isOutOfBounds || isOverlapping) {
           canPlaceShip = false;
           break;
         }
       }
-  
+
       if (canPlaceShip) {
-        for (let i = 0; i < shipLength; i++) { 
+        for (let i = 0; i < shipLength; i++) {
           const newRow = row + i * rowDir;
           const newCol = col + i * colDir;
-  
+
           newShips.push({ type: "Computer Ship", row: newRow, col: newCol, length: shipLength });
         }
         return newShips;
       }
     }
-  
+
     return ships;
   };
 
   const initializeGame = () => {
-    
-      // Initialize the game for the computer's turn (randomly place computer ships)
-      const randomShips = generateRandomShips();
-      let ships = randomShips;
-  
-      for (let i = 0; i < ships.length; i++) {
-        ships = handlePlaceShip(
-          ships[i].row,
-          ships[i].col,
-          ships[i].length,
-          ships
-        );
-      }
-  
-      coordinatesRef.current = [];
-      setShips(ships);
-      
-    
+
+    // Initialize the game for the computer's turn (randomly place computer ships)
+    const randomShips = generateRandomShips();
+    let ships = randomShips;
+
+    for (let i = 0; i < ships.length; i++) {
+      ships = handlePlaceShip(
+        ships[i].row,
+        ships[i].col,
+        ships[i].length,
+        ships
+      );
+    }
+
+    coordinatesRef.current = [];
+    setShips(ships);
+
+
   };
 
   const renderBoard = () => {
     const boardElements = [];
     for (let row = 0; row < 10; row++) {
-        const columns = [];
-        for (let col = 0; col < 10; col++) {
-            const isSquareClicked = coordinatesRef.current.some(coord => coord[0] === row && coord[1] === col);
-            const shipAtPosition = ships.find((ship) => ship.row === row && ship.col === col);
-            const shipClass = shipAtPosition ? (shipAtPosition.status === 'hit' ? 'hit-ship' : shipAtPosition.ship.name) : '';       
+      const columns = [];
+      for (let col = 0; col < 10; col++) {
+        const isSquareClicked = coordinatesRef.current.some(coord => coord[0] === row && coord[1] === col);
 
-            let squareClass = 'square'; 
-            if (isSquareClicked) {
-                if (shipAtPosition && shipAtPosition.status === 'hit') {
-                    squareClass = 'square hit ship';
-                } else {
-                    squareClass = 'square missed'; 
-                }
+        let shipAtPosition;
+        for (let ship of ships) {
+          for (let i = 0; i < ship.length; i++) {
+            if (ship.row + i === row && ship.col === col) {
+              shipAtPosition = ship;
+              break;
             }
-        
-            columns.push(
-                <div
-                    key={`${row}-${col}`} 
-                    className={`${squareClass} ${shipClass}`}
-                    onClick={() => handleSquareClick(row, col)}
-                ></div>
-            );
+          }
+          if (shipAtPosition) break;
         }
-        boardElements.push(<div className="board-row" key={row}> {columns} </div>);
+
+        const shipClass = shipAtPosition ? shipAtPosition.ship.name : '';
+
+        let squareClass = 'square';
+        if (isSquareClicked) {
+          if (shipAtPosition && shipAtPosition.status === 'hit') {
+            // If the ship is hit, then only the class 'hit' is added
+            squareClass += ` ${shipClass} hit`;
+          } else {
+            // If no ship is hit, only 'missed' is added
+            squareClass += ` ${shipClass} missed`;
+          }
+        }
+
+
+        columns.push(
+          <div
+            key={`${row}-${col}`}
+            className={`${squareClass} ${shipClass}`}
+            onClick={() => handleSquareClick(row, col)}
+          ></div>
+        );
+      }
+      boardElements.push(<div className="board-row" key={row}> {columns} </div>);
     }
+
     return boardElements;
-};
+  };
+
 
   useEffect(() => {
-      initializeGame();
+    initializeGame();
   }, []);
 
 
   return <div className="board">
-  {renderBoard()}
-</div>;
+    {renderBoard()}
+  </div>;
 
 }
